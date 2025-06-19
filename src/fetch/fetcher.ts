6896261707a39b3f859c8dda49ada6626566c7a8
takoway <ikanohiza@gmail.com>
2025-06-19 15:34:27 +0900

    axiosをインストールしてfetcher追加

---
+++ src/fetch/fetcher.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosError } from "axios";
+
+// APIエンドポイント
+export const EP = {
+    
+}
+
+export const fetcherGet = async <T>(url: string): Promise<T> =>
+    axios.get<T>(url)
+        .then((res: { data: T }) => res.data)
+        .catch((err: AxiosError) => {
+            if (err?.message?.startsWith("Network Error")) {
+                throw { message: "Network Error" };
+            }
+            throw err.response;
+        });
+
+export const fetcherPost = <T>(url: string, body: object = {}) =>
+    axios.post<T>(url, body)
+        .then((res: { data: T }) => {
+            return {data: res.data, err: undefined}
+        })
+        .catch((err: AxiosError) => {
+            return {data: null, err: err.response}
+        })
+
+export const fetcherPut = <T>(url: string, body: object = {}) =>
+    axios.put<T>(url, body)
+        .then((res: import("axios").AxiosResponse<T>) => {
+            return {data: res, err: undefined}
+        })
+        .catch((err: AxiosError) => {
+            return {data: undefined, err: err.response}
+        })
+
+export const fetcherDelete = <T>(url: string) =>
+    axios.delete<T>(url)
+        .then((res: import("axios").AxiosResponse<T>) => {
+            return { data: res.data, err: undefined };
+        })
+        .catch((err: AxiosError) => {
+            return { data: undefined, err: err.response };
+        });
